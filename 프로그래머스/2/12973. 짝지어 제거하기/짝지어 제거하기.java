f6d78ecb7235618bddaebcee9e77c5fc79013062
Ban-gilhyeon <163241691+Ban-gilhyeon@users.noreply.github.com>
2024-05-31 07:25:34 +0900

    [level 2] Title: 짝지어 제거하기, Time: 107.81 ms, Memory: 84.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+class Solution
+{
+  Stack<String> st = new Stack<>();
+    public int removePair(char[] arr){
+        for(int i = 0; i < arr.length; i++){
+            if((i != arr.length-1 && arr[i] != arr[i+1])||i == arr.length-1){
+                if( st.size() >= 1 && st.peek().equals(String.valueOf(arr[i]))){
+                    st.pop();
+                }
+                else st.add(arr[i]+"");
+            }
+            else ++i;
+        }
+        if(st.empty()) return 1;
+        return 0;
+    }
+    public int solution(String s){
+        int answer = -1;
+        char[] arr = s.toCharArray();
+        answer = removePair(arr);
+        return answer;
+    }
+}

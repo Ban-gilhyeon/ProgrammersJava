04190fb2fa9c429d41be97cecad668093705c1cc
Ban-gilhyeon <163241691+Ban-gilhyeon@users.noreply.github.com>
2024-06-17 02:23:27 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.09 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+     int answer =0 ;
+      int [] arr = new int[n+1];
+      int cnt = 1;
+      String num = Integer.toString(n);
+      for(int i = 1; i<= n; i++){
+        while(cnt % 3 == 0 || Integer.toString(cnt).contains("3")){
+            cnt++;
+        }
+        arr[i] = cnt++;
+      }
+      answer = arr[n];
+      return answer;
+    }
+}

8d099a310dd381c6258ee905dd4aa76e0e826cf5
Ban-gilhyeon <163241691+Ban-gilhyeon@users.noreply.github.com>
2024-06-19 03:52:44 +0900

    [level 2] Title: 하노이의 탑, Time: 4.55 ms, Memory: 108 MB -BaekjoonHub

---
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    List<int[]>arr;
+    
+    public void dfs(int n, int start, int mid, int end){
+        //n-1 이 mid
+        // 나머지 1 start -> end
+        //짝수 mid , 홀수 end
+        if(n == 1) {
+            arr.add(new int[]{start, end});
+            return;
+        }
+        dfs(n-1, start ,end, mid);
+        arr.add(new int[]{start, end});
+        dfs(n-1,mid, start ,end);
+    }
+     public int [][] solution(int n){
+        this.arr = new ArrayList<>();
+        dfs(n,1,2,3);
+        int [][] answer = new int[arr.size()][];
+        for(int i = 0; i < arr.size(); i++){
+            answer[i] = arr.get(i);
+        }
+        return answer;
+    }
+}
